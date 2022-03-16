5a0e121753cff63a640b5efa78df5dc40f8dfe53
tanyinghui <yhtan927@gmail.com>
2022-03-16 23:52:24 +0900

    black formatter

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Variational Autoencoders (VAE)
+[![CodeFactor](https://www.codefactor.io/repository/github/lunayht/vae/badge)](https://www.codefactor.io/repository/github/lunayht/vae) [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
 ## Requisites
 * Linux/ OSX system with FFmpeg and imagemagick
--- utils.py
+++ utils.py
@@ -1,14 +1,17 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 def save_gif(progress_images, filename):
     fig = plt.figure()
-    
+
     ims = []
     for i in range(len(progress_images)):
         im = plt.imshow(progress_images[i], animated=True, cmap="gray")
         ims.append([im])
 
-    ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True, repeat_delay=1000)
-    
+    ani = animation.ArtistAnimation(
+        fig, ims, interval=100, blit=True, repeat_delay=1000
+    )
+
     ani.save(filename, writer="imagemagick")
